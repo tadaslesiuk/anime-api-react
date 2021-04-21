ed98a4535b6c0b304b9d07deb2d21206d06b213c
tadaslesiuk <tadaslesiuk@gmail.com>
2021-04-21 23:59:55 +0300

    Added Bootstrap and changed design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,12 @@
+import Logo from '../../assets/images/logo.png';
+
 const Header = () => {
     return (
         <header>
-            <h1>The<strong>Anime</strong>Database</h1>
+            <div className="row justify-content-start ml-2">
+                <img src={Logo} alt="Logo" height="150px" />
+                {/* <h1>The<strong>Anime</strong>Database</h1> */}
+            </div>
         </header>
     );
 }
